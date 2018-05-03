bc4f311b0f053d9b410ede3d8d75077ca167a5de
Fabio Busatto <fabio@gitlab.com>
2018-05-03 08:18:02 +0000

    Fix security issues

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class DemoApplication {
 
-	@RequestMapping("/")
+	@GetMapping("/")
 	String home() {
 		return "Spring is here!";
 	}
